a01a86618fc45a6cbde533a6b409a5504e008ce4
Olotuah <gbengaolotuah@gmail.com>
2025-05-02 22:14:23 +0100

    Fix PWA manifest and icon sizes for correct installation and display

--- vite.config.js
+++ vite.config.js
@@ -20,15 +20,20 @@ export default defineConfig({
         start_url: '/',
         icons: [
           {
-            src: 'icon-192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: 'icon-512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          }
+      "src": "/icon-512.png",
+      "sizes": "192x192",
+      "type": "image/png"
+    },
+    {
+      "src": "/icon-192png",
+      "sizes": "512x512",
+      "type": "image/png"
+    },
+    {
+      "src": "/icon-193.png",
+      "sizes": "144x144",
+      "type": "image/png"
+    }
         ]
       }
     })
